b71b7b0323db8325cc14b0019797a634957e493d
Gustavo Pinto <ggustavohenriquepinto45@gmail.com>
2024-07-12 23:10:39 +0000

    Exercício "Transportes", OBI 2023, Fase 2
    Encontra-se incompleto

---
+++ 2023/fase 3/transportes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define T 100000
+int n,m,k;
+int p[T],v[T],u[T],t[T];
+int a,b,ax,bx,tx;
+
+int main(void) {
+  scanf("%d %d %d",&n,&m,&k);
+  for(int i=0;i<k;i++)scanf("%d",&p[i]);
+  for(int i=0;i<m;i++)scanf("%d %d %d",&v[i],&u[i],&t[i]);
+  scanf("%d %d",&a,&b); 
+
+  //Impossível?  
+
+  return 0;
+}

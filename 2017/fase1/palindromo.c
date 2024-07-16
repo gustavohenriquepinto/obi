87ac9a3d08f989a7556f836f89f73e495609b73f
Gustavo Pinto <ggustavohenriquepinto45@gmail.com>
2024-07-16 14:27:28 -0300

    Inocompleto

---
+++ 2017/fase1/palindromo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int palindromo(char* s,int n){
+    for(int i=0;i<n;i++)
+        if(s[i]!=s[n-1-i])
+            return 0;
+    return 1;
+}
+int main(void){
+    int n;scanf("%d",&n);
+    int s[n];scanf("%s",s);
+
+    int x=0;
+    for(int i)
+
+    return 0;
+}

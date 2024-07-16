6f4f425b41463ed26fb6f40f8203b7a3ad26b33a
Gustavo Pinto <ggustavohenriquepinto45@gmail.com>
2024-07-16 00:53:22 +0000

    Exercício "Subcadeias", OBI 2022, Fase 2
    Primeiramente, realizamos uma iteração de percorrendo toda a cadeia de caracteres, e então para cada caractere, verificamos a existência de um palíndromo a partir da posição dada.

--- 2022/fase2/subcadeias.c
+++ 2022/fase2/subcadeias.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 
-int palindromo(char s[],int x,int y){
-    for(int i=x;i<=y;i++)
-        if(s[i]!=s[y+x-i])
-            return 0;
-    return 1;
+int palindromo(char s[],int n){
+  for(int i=0;i<n/2+1;i++)
+    if(s[i]!=s[n-1-i])
+      return 0;
+  return 1;
 }
 
 int main(void){
-    int n,m=1;scanf("%d",&n);
-    char s[n];scanf("%s",s);
+  int n,m=1;scanf("%d",&n);
+  char s[n];scanf("%s",s);
 
-    for(int i=0;i<n;i++){
-        int r=n-i-1;
-        for(int j=n-1;j>i;j--){
-            if(palindromo(s,i,j))break;
-            r--;
-        }
-        if(m<r)m=r;
+  for(int i=0;i<n;i++){
+    if(m+i>=n-1)break;
+    int r=n-i;
+    for(int j=n-1;j>i;j--){
+      if(palindromo(&s[i],j-i+1))break;
+      r--;
     }
+    if(m<r)m=r;
+  }
 
-    printf("%d",m);
-
-    return 0;
+  printf("%d",m);
+  
+  return 0;
 }
